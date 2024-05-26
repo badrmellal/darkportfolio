1fd2ee0f98aa1f75b56dd58e52f9bba927a5ab12
badrmellal <contact.moddy@gmail.com>
2024-05-26 08:42:07 +0100

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { BsArrowRight } from "react-icons/bs";
 import { RiSendPlaneFill } from "react-icons/ri";
+import Notification from './Notification';
 
 
 const Contact = () => {
+    const [showNotification, setShowNotification] = useState(false);
   const [formData, setFormData] = useState({
     email : '',
     username : '',
@@ -33,7 +35,13 @@ const Contact = () => {
 
       if (response.ok) {
         // Form submitted successfully
+        setShowNotification(true);
         console.log('Form submitted successfully');
+        setFormData({
+          email: '',
+          username: '',
+          message: '',
+        });
       } else {
         // Form submission error
         console.error('Form submission failed');
@@ -45,11 +53,16 @@ const Contact = () => {
 
   return (
     <div id="contact" className="container m-auto mt-16">
+     {showNotification && (
+        <Notification
+          message= "Succefully Sent"
+          description="I will get back to you in less than 24h."
+          onClose={() => setShowNotification(false)}
+        />
+      )}
       {/* heading */}
-      <div 
-       data-aos="fade-up"
-       className="relative mb-5">
-        <h3 className=" text-3xl font-black text-indigo-500 sm:text-2xl">
+      <div data-aos="fade-up" className="relative mb-5">
+        <h3 className="text-3xl font-black text-indigo-500 sm:text-2xl">
           Contact
         </h3>
         <span className="h-[1.1px] right-0 absolute w-[92%] bg-gray-300 block"></span>
@@ -58,22 +71,18 @@ const Contact = () => {
       {/* card*/}
       <div className="card-wrapper w-[90%] sm:w-[100%] mx-auto mt-5 flex items-center justify-center sm:flex-col">
         <div className="left w-[70%] flex-1 flex items-center justify-center sm:flex-col sm:w-full">
-          <div className="flex-3 w-1/2 gap-3 flex items-end justify-end  flex-col sm:w-3/4">
-            <div 
-            data-aos="zoom-in"
-            >
+          <div className="flex-3 w-1/2 gap-3 flex items-end justify-end flex-col sm:w-3/4">
+            <div data-aos="zoom-in">
               <h1 className="text-5xl text-white font-bold sm:text-3xl">You Need</h1>
-              <h3 className="text-xl text-indigo-300  sm:text-lg">
+              <h3 className="text-xl text-indigo-300 sm:text-lg">
                 A web or mobile app leave a request here, it's FREE
               </h3>
             </div>
           </div>
-          <div className=" flex p-5 items-center justify-center ">
+          <div className="flex p-5 items-center justify-center">
             <button
-              
               data-aos="zoom-in"
-              
-              className= " text-indigo-400 font-extrabold text-3xl p-2 rounded-lg shadow-[0 0 10px 1px rgba(255, 255, 255, 0.1)] "
+              className="text-indigo-400 font-extrabold text-3xl p-2 rounded-lg shadow-[0 0 10px 1px rgba(255, 255, 255, 0.1)]"
             >
               <BsArrowRight className="transform md:rotate-0 group hover:rotate-180 transition-transform duration-300 ease-in-out cursor-pointer" />
             </button>
@@ -81,11 +90,8 @@ const Contact = () => {
         </div>
         <div className="right flex-1">
           <form
-            
             data-aos="zoom-in"
-            
             className="flex justify-center items-center flex-col gap-5 w-[70%] md:w-[100%] sm:w-[95%] mx-auto"
-            action="mailto:contact.moddy@gmail.com"
             onSubmit={handleSubmit}
           >
             <input
@@ -112,14 +118,13 @@ const Contact = () => {
               value={formData.message}
               onChange={handleInputChange}
               name="message"
-              id=""
             />
             <button
-              className="bg-indigo-500 hover:bg-indigo-600 w-full text-white font-semibold  p-2 rounded-lg flex items-center justify-center space-x-1"
+              className="bg-indigo-500 hover:bg-indigo-600 w-full text-white font-semibold p-2 rounded-lg flex items-center justify-center space-x-1"
               type="submit"
             >
               <span>Send</span>
-              <RiSendPlaneFill/>
+              <RiSendPlaneFill />
             </button>
           </form>
         </div>
